d9fa232e8adf3c2508d353a95803efd92204a4c5
Marin Zaimov <marin@gatech.edu>
2012-11-04 17:21:52 -0500

    asdasdasd

--- application/protected/views/layouts/main.php
+++ application/protected/views/layouts/main.php
@@ -112,11 +112,6 @@
   </div><!-- /.navbar-inner -->
 </div><!-- /.navbar -->
 
-<div class="container" id="page">
-
-	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
-	</div><!-- header -->
 
 	<?php if(isset($this->breadcrumbs)):?>
 		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
